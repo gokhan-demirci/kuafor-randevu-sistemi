75a58da1709ffbf8646aa89a9d962699bb26e4bd
Can yilmaz <github-sayitcanyilmaz65@gmail.com>
2026-05-29 18:33:57 +0300

    Fix backend endpoint and admin api scripts

--- src/main/java/com/kuaforrys/kuafor_backend/controller/RandevuController.java
+++ src/main/java/com/kuaforrys/kuafor_backend/controller/RandevuController.java
@@ -1,12 +1,14 @@
 package com.kuaforrys.kuafor_backend.controller;
 
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
 import com.kuaforrys.kuafor_backend.entity.Randevu;
 import com.kuaforrys.kuafor_backend.service.RandevuService;
 
 @RestController
 @RequestMapping("/api/randevu")
-@CrossOrigin("*") 
+@CrossOrigin("*")
 public class RandevuController {
 
     private final RandevuService service;
@@ -15,13 +17,13 @@ public class RandevuController {
         this.service = service;
     }
 
-    
     @PostMapping("/al")
     public Randevu randevuAl(@RequestBody Randevu randevu) {
         return service.randevuOlustur(randevu);
     }
-}
-@GetMapping("/liste")
-public java.util.List<Randevu> randevuListesi() {
-    return service.tumRandevulariGetir();
+
+    @GetMapping("/liste")
+    public List<Randevu> randevuListesi() {
+        return service.tumRandevulariGetir();
+    }
 }
